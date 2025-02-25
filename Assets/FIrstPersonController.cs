5565c73dd4efad526b2538820138ee19f13617a1
Fubar <116589378+CMTHL@users.noreply.github.com>
2025-02-25 14:02:17 +0100

    FPScontroller (New Input System)

--- Assets/FIrstPersonController.cs
+++ Assets/FIrstPersonController.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class FIrstPersonController : MonoBehaviour
 {
@@ -16,25 +18,52 @@ public class FIrstPersonController : MonoBehaviour
     [SerializeField] private float mouseSensitivity = 2.0f;
     [SerializeField] private float upDownRange = 80.0f;
     
-    [Header("Imputs Customization")]
-    [SerializeField] private string horizontalMoveInput = "Horizontal";
-    [SerializeField] private string verticalMoveInput = "Vertical";
-    [SerializeField] private KeyCode sprintKey = KeyCode.LeftShift; 
-    [SerializeField] private KeyCode jumpKey = KeyCode.Space;
-    [SerializeField] private string mouseXInput = "Mouse X";
-    [SerializeField] private string mouseYInput = "Mouse Y";
+    [Header("Input Actions")]
+    [SerializeField] private InputActionAsset playerControlls;
+    
+    private InputAction _moveAction;
+    private InputAction _jumpAction;
+    private InputAction _sprintAction;
+    private InputAction _lookAction;
+    private Vector2 _moveInput;
+    private Vector2 _lookInput;
     
     private Camera _mainCamera;
     private float _verticalRotation;
     private Vector3 _currentMovement = Vector3.zero;
     private CharacterController _characterController;
 
-    private void Start()
+    private void Awake()
     {
         _characterController = GetComponent<CharacterController>();
         _mainCamera = Camera.main;
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+         
+        _moveAction = playerControlls.FindActionMap("Player").FindAction("Move");
+        _jumpAction = playerControlls.FindActionMap("Player").FindAction("Jump");
+        _sprintAction = playerControlls.FindActionMap("Player").FindAction("Sprint");
+        _lookAction = playerControlls.FindActionMap("Player").FindAction("Look");
+        
+        _moveAction.performed += context => _moveInput = context.ReadValue<Vector2>();
+        _moveAction.canceled += context => _moveInput = Vector2.zero;
+        
+        _lookAction.performed += context => _lookInput = context.ReadValue<Vector2>();
+        _lookAction.canceled += context => _lookInput = Vector2.zero;
+    }
+
+    private void OnEnable()
+    {
+        _moveAction.Enable();
+        _jumpAction.Enable();
+        _sprintAction.Enable();
+        _lookAction.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _moveAction.Disable();
+        _jumpAction.Disable();
+        _sprintAction.Disable();
+        _lookAction.Disable();
     }
 
     private void Update()
@@ -45,18 +74,20 @@ public class FIrstPersonController : MonoBehaviour
 
     void HandleRotation()
     {
-        float mouseXRotation = Input.GetAxis(mouseXInput) * mouseSensitivity;
+        Debug.Log($"Look Input Y: {_lookInput.y}");
+        float mouseXRotation = _lookInput.x * mouseSensitivity;
         transform.Rotate(0, mouseXRotation, 0);
         
-        _verticalRotation -= Input.GetAxis(mouseYInput) * mouseSensitivity;
+        _verticalRotation -= _lookInput.y * mouseSensitivity;;
         _verticalRotation = Mathf.Clamp(_verticalRotation, -upDownRange, upDownRange);
         _mainCamera.transform.localRotation = Quaternion.Euler(_verticalRotation, 0, 0);
     }
+    
     void HandleMovement()
     {
-        float speedMultiplier = Input.GetKey(sprintKey) ? sprintMultiplier : 1f;
-        float verticalSpeed = Input.GetAxis(verticalMoveInput) * walkSpeed * speedMultiplier;
-        float horizontalSpeed = Input.GetAxis(horizontalMoveInput) * walkSpeed * speedMultiplier;
+        float speedMultiplier = _sprintAction.ReadValue<float>() > 0 ? sprintMultiplier : 1f;
+        float verticalSpeed = _moveInput.y * walkSpeed * speedMultiplier;
+        float horizontalSpeed = _moveInput.x * walkSpeed * speedMultiplier;
         
         
         Vector3 horizontalMovement = new Vector3(horizontalSpeed, 0, verticalSpeed);
@@ -68,21 +99,18 @@ public class FIrstPersonController : MonoBehaviour
         _currentMovement.z = horizontalMovement.z;
         
         _characterController.Move(_currentMovement * Time.deltaTime);
-    }
+    } 
+    
     void HandleJumpAndGravity()
     {
         if (_characterController.isGrounded)
         {
             _currentMovement.y = -0.5f;
-            if (Input.GetKeyDown(jumpKey))
+            if (_jumpAction.triggered)
             {
-                _currentMovement.y = jumpForce;
+                _currentMovement.y = Mathf.Sqrt(2 * jumpForce * gravity);
             }
         }
-        else
-        {
-            _currentMovement.y -= gravity * Time.deltaTime;
-        }
-
+        _currentMovement.y -= gravity * Time.deltaTime;
     }
 }
